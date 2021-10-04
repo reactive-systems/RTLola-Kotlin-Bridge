31a36292646d3e458db51240f9a8f28656c882ab
Maximilian Schwenger <maximilian.schwenger@cispa.de>
2021-10-04 12:15:11 +0200

    Define single/partial/total event transmission.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,8 @@
 use core::time::Duration;
+use jni::errors::Result;
 use jni::objects::{JObject, JString};
 use jni::strings::JavaStr;
-use jni::sys::{jdouble, jdoubleArray, jint, jlong};
+use jni::sys::{jbooleanArray, jdouble, jdoubleArray, jint, jlong};
 use jni::JNIEnv;
 use ordered_float::NotNan;
 use rtlola_frontend::ParserConfig;
@@ -55,11 +56,6 @@ pub extern "C" fn init(
     Box::into_raw(Box::new(monitor))
 }
 
-unsafe fn convert_java_str(js: JavaStr) -> String {
-    let raw = CStr::from_ptr(js.as_ptr());
-    String::from(raw.to_str().unwrap())
-}
-
 /// Receives a single event and returns an array of verdicts.
 ///
 /// Interprets the `monitor` input as pointer to a `KotlinMonitor` received via the `init` function.
@@ -74,10 +70,9 @@ pub extern "C" fn receive_single_value(
     timestamp: jdouble,
 ) -> jdoubleArray {
     let mut mon = unsafe { Box::from_raw(monitor as *mut KotlinMonitor) };
-    let mut event = vec![0f64; mon.num_inputs + 1];
-    event[input_ix as usize] = value;
-    event[mon.num_inputs + 1] = timestamp;
-    process_event(env, &mut mon, &event)
+    let mut event = vec![Value::None; mon.num_inputs];
+    event[input_ix as usize] = value_from_f64(value);
+    process_event(env, &mut mon, &event, timestamp)
 }
 
 /// Receives a single event and returns an array of verdicts.
@@ -85,7 +80,7 @@ pub extern "C" fn receive_single_value(
 /// Interprets the `monitor` input as pointer to a `KotlinMonitor` received via the `init` function.
 /// The `input` argument contains a long value for each input of the specification plus the current timestamp at the end.
 #[no_mangle]
-pub extern "C" fn receive_event(
+pub extern "C" fn receive_total_event(
     env: JNIEnv,
     _: JObject,
     monitor: jlong,
@@ -93,26 +88,83 @@ pub extern "C" fn receive_event(
 ) -> jdoubleArray {
     let mut mon = unsafe { Box::from_raw(monitor as *mut KotlinMonitor) };
     let num_values = mon.num_inputs + 1;
-    let mut event = vec![0.0; num_values];
-    let copy_res = env.get_double_array_region(inputs, 0, &mut event);
+    let inputs = get_floats(env, inputs, num_values);
 
-    debug_assert!(copy_res.is_ok());
-    if copy_res.is_err() {
+    debug_assert!(inputs.is_ok());
+    if inputs.is_err() {
         // In release config, ignore invalid inputs.
         return env.new_double_array(0).unwrap();
     }
-    process_event(env, &mut mon, &event)
+    let inputs = inputs.unwrap();
+    let (time, inputs) = inputs.split_last().unwrap();
+    let inputs = inputs
+        .iter()
+        .copied()
+        .map(value_from_f64)
+        .collect::<Vec<_>>();
+    process_event(env, &mut mon, &inputs, *time)
 }
 
-fn process_event(env: JNIEnv, mon: &mut KotlinMonitor, event: &[f64]) -> jdoubleArray {
-    let (time, input) = event.split_last().unwrap();
-    let input: Vec<Value> = input
+/// Receives a single event and returns an array of verdicts.
+///
+/// Interprets the `monitor` input as pointer to a `KotlinMonitor` received via the `init` function.
+/// The `input` argument contains a long value for each input of the specification plus the current timestamp at the end.
+/// The `active` argument is a bool array where a `true` value at position `ix` indicates that the `ix`th value of
+/// `input` contains a meaningful new value.  All other values will be ignored.
+/// The timestamp must always be active, so the following invariant must hold:
+/// `len(inputs) == len(active) && last(active) || len(inputs) == len(active) + 1
+#[no_mangle]
+pub extern "C" fn receive_partial_event(
+    env: JNIEnv,
+    _: JObject,
+    monitor: jlong,
+    inputs: jdoubleArray,
+    active: jbooleanArray,
+) -> jdoubleArray {
+    let mut mon = unsafe { Box::from_raw(monitor as *mut KotlinMonitor) };
+    let num_values = mon.num_inputs + 1;
+
+    let inputs = get_floats(env, inputs, num_values);
+    let active = get_bools(env, active, num_values);
+    // crash in debug
+    debug_assert!(inputs.is_ok());
+    debug_assert!(active.is_ok());
+    if active.is_err() || inputs.is_err() {
+        // In release config, ignore invalid inputs.
+        return env.new_double_array(0).unwrap();
+    }
+    let inputs = inputs.unwrap();
+    let (time, input) = inputs.split_last().unwrap();
+    let active = active.unwrap();
+
+    let event: Vec<Value> = input
         .iter()
-        .map(|f| Value::Float(NotNan::new(*f).unwrap()))
+        .zip(active)
+        .map(|(f, a)| if a { value_from_f64(*f) } else { Value::None })
         .collect();
+    process_event(env, &mut mon, &event, *time)
+}
+
+fn get_floats(env: JNIEnv, inputs: jdoubleArray, num_values: usize) -> Result<Vec<f64>> {
+    let mut event = vec![0.0; num_values];
+    env.get_double_array_region(inputs, 0, &mut event)?;
+    Ok(event)
+}
+
+fn get_bools(
+    env: JNIEnv,
+    inputs: jdoubleArray,
+    num_values: usize,
+) -> Result<impl Iterator<Item = bool>> {
+    let mut values = vec![0u8; num_values];
+    env.get_boolean_array_region(inputs, 0, &mut values)?;
+    Ok(values.into_iter().map(|v| v != 0))
+}
+
+fn process_event(env: JNIEnv, mon: &mut KotlinMonitor, event: &[Value], time: f64) -> jdoubleArray {
     let updates = mon
         .monitor
-        .accept_event(input, Duration::new(time.floor() as u64, 0));
+        .accept_event(event, Duration::new(time.floor() as u64, 0));
 
     let num_updates = updates.timed.len();
     let res = env
@@ -140,4 +192,13 @@ fn process_event(env: JNIEnv, mon: &mut KotlinMonitor, event: &[f64]) -> jdouble
             });
     debug_assert!(output_copy_res.is_ok());
     res
+}
+
+fn value_from_f64(v: f64) -> Value {
+    Value::Float(NotNan::new(v).unwrap())
+}
+
+unsafe fn convert_java_str(js: JavaStr) -> String {
+    let raw = CStr::from_ptr(js.as_ptr());
+    String::from(raw.to_str().unwrap())
 }
